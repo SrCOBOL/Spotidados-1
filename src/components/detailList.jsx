85f8e15972356c6aa0c71abed891357d3298876f
SrCOBOL <kikobborges@gmail.com>
2025-01-02 09:51:37 +0000

    Update detailList.jsx

--- src/components/detailList.jsx
+++ src/components/detailList.jsx
@@ -1,23 +1,58 @@
-import React from 'react';
+import React from "react";
 
 const DetailList = ({ filters, grafList }) => {
   if (!filters.topFilter) {
-    return
+    return null;
   }
 
   return (
-    <table style={{ width: '100%', margin: '20px auto', backgroundColor: 'rgb(34, 34, 34)', color: '#fff', borderCollapse: 'collapse',fontFamily: "Afacad" }}>
+    <table
+      style={{
+        width: "100%",
+        margin: "20px auto",
+        backgroundColor: "rgb(34, 34, 34)",
+        color: "#fff",
+        borderCollapse: "collapse",
+        fontFamily: "Afacad",
+        borderRadius: "10px",
+        overflow: "hidden",
+      }}
+    >
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Reproductions</th>
+          <th
+            style={{
+              textAlign: "center",
+              padding: "10px",
+              borderTopLeftRadius: "10px",
+              borderTopRightRadius: "10px",
+            }}
+          >
+            Name
+          </th>
+          <th
+            style={{
+              textAlign: "center",
+              padding: "10px",
+              borderTopRightRadius: "10px",
+            }}
+          >
+            Reproductions
+          </th>
         </tr>
       </thead>
       <tbody>
         {grafList.map((item, index) => (
-          <tr key={index} style={{ textAlign: 'center', fontFamily:"Afacad" }}>
+          <tr
+            key={index}
+            style={{
+              textAlign: "center",
+              borderBottom:
+                index === grafList.length - 1 ? "none" : "1px solid white",
+            }}
+          >
             <td>{item.name}</td>
-            <td>{item.value}</td>
+            <td style={{ padding: "10px" }}>{item.value}</td>
           </tr>
         ))}
       </tbody>
